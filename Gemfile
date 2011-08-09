0c06de4fe9765f235cbaf74c504855ec47421c28
mzp <mzpppp@gmail.com>
2011-08-09 22:09:55 +0900

    program for coderz

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'http://rubygems.org'
+
+gem 'sinatra'
+gem 'sass'
+gem 'haml'
+
+group :development, :test do
+  gem 'shotgun'
+end
---
+++ app.rb
@@ -0,0 +1,8 @@
+#! /opt/local/bin/ruby -w
+# -*- mode:ruby; coding:utf-8 -*-
+require 'sinatra'
+enable :run
+
+get '/' do
+  "program for coderz"
+end
